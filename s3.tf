d79d1b0c1973da72940598a7ca357d0c8fa8e1ee
srikanth-pole <srikanthgoud2308@gmail.com>
2024-10-25 17:04:44 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "one" {
-  bucket = "mustafaflm.devops.project.bucket"
+  bucket = "srikanthflm.devops.project.bucket"
 }
 
 resource "aws_s3_bucket_ownership_controls" "two" {
@@ -27,7 +27,7 @@ status = "Enabled"
 terraform {
 backend "s3" {
 region = "us-east-1"
-bucket = "mustafaflm.devops.project.bucket"
+bucket = "srikanthflm.devops.project.bucket"
 key = "prod/terraform.tfstate"
 }
 }
