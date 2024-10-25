539277c7397d60b0f1825e5b6ae83a2c9bc40252
srikanth-pole <srikanthgoud2308@gmail.com>
2024-10-25 17:09:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "aws_launch_template" "web_server_as" {
     name = "myproject"
-    image_id           = "ami-00f251754ac5da7f0"
+    image_id           = "ami-0ddc798b3f1a5117e"
     instance_type = "t2.micro"
-    key_name = "mustafakey-pair"
+    key_name = "IBACO"
     
 }
    
@@ -11,7 +11,7 @@ resource "aws_launch_template" "web_server_as" {
   resource "aws_elb" "web_server_lb"{
      name = "web-server-lb"
      security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-0883b34c988bcbf20", "subnet-0f4f4322d734fcc38"]
+     subnets = ["subnet-0d9797a807e5ac77a", "subnet-04b66c733c2de751d"]
      listener {
       instance_port     = 8000
       instance_protocol = "http"
